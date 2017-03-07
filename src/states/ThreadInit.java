a800883941259562d38e292e168500114d7e2aa3
sayig86 <sayig86@gmail.com>
2017-03-07 19:49:47 +0100

    Documentation
    Documentation of Hashmap

--- src/states/ThreadInit.java
+++ src/states/ThreadInit.java
@@ -32,6 +32,12 @@ public class ThreadInit implements ThreadState {
      * @return none
      * @params none
      */
+
+    /**
+     *@author Samet
+     * @return none
+     * @params Deserialize and HashMap
+     * */
     @Override
     public void initializeClientObject() {
         Arduino arduino = new Arduino();
@@ -48,35 +54,55 @@ public class ThreadInit implements ThreadState {
 
             message = "samet#togkoer,defaultValue,minValue,maxValue,currentValue,unitName,unitCount#togkoer,defaultValue,minValue,maxValue,currentValue,unitName,unitCount#" ;
 
+            // Split message by "#"
             String[] parts = message.split("#");
-            arduino.setName(parts[0]);
 
+            //Set the Arduino name
+            arduino.setName(parts[0]);
+            //This method takes an array and converts it to a List object.
             List<String> methods = new ArrayList<String>(Arrays.asList(parts));
 
+            //Removing the name
             methods.remove(0);
 
+            // Creating a hashmap for group and core methods
              Map<String, ArduinoMethod> groupMethods = new HashMap<>();
              Map<String, ArduinoMethod> coreMethods = new HashMap<>();
+
+
             for (String methodString : methods)
             {
+                //Split var by ","
                 String [] var = methodString.split(",");
+
+                //checking currentState are null or empty
                 if(var[5] != null || var[5] != "")
                 {
+                    //If empty or null put into groupMethods
                     groupMethods.put(var[0], new ArduinoMethod(var[0],Integer.parseInt(var[1]),Integer.parseInt(var[2]),Integer.parseInt(var[3]),Integer.parseInt(var[4]),var[5], var[6]));
 
 
                 }
                 else
                 {
+                    //Else put into coreMethods
                     coreMethods.put(var[0], new ArduinoMethod(var[0],Integer.parseInt(var[1]),Integer.parseInt(var[2]),Integer.parseInt(var[3]),Integer.parseInt(var[4]),var[5], var[6]));
                 }
 
             }
+            // Set core & groupmethods
             arduino.setCoreMethods(coreMethods);
             arduino.setGroupMethods(groupMethods);
+
+            //Get and Set the ip and converte the ip to a string
             arduino.setIp(client.getSocket().getInetAddress().toString());
+
+            // Creating new hashmap for connectet arduino client
             Map<String, Arduino> arduoinoClient = new HashMap<>();
+
+
             arduoinoClient.put(arduino.getIp(), arduino);
+            // Get instance and set clients in arduinoClient
             ArduinoServer.getInstance().setClients(arduoinoClient);
         }
         catch (Exception e)
