e516b209d9236529787f787a7f3691af2b8554c1
Michael Skjaerlund <michael_b_hansen@live.dk>
2017-03-08 09:12:59 +0100

    Created deSerialize method

---
+++ src/states/Heartbeat.java
@@ -0,0 +1,24 @@
+package states;
+
+import interfaces.ASState;
+
+/**
+ * Created by Michael Skjærlund on 08-03-2017.
+ */
+public class Heartbeat implements ASState {
+
+    @Override
+    public void initializeSocket() {
+
+    }
+
+    @Override
+    public void listening() {
+
+    }
+
+    @Override
+    public void reboot() {
+
+    }
+}
--- src/states/ThreadInit.java
+++ src/states/ThreadInit.java
@@ -40,7 +40,7 @@ public class ThreadInit implements ThreadState {
      * */
     @Override
     public void initializeClientObject() {
-        Arduino arduino = new Arduino();
+
 
 
         //TODO method read code here?
@@ -52,59 +52,9 @@ public class ThreadInit implements ThreadState {
             //Confirms that the message was received
             System.out.println(message);
 
-            message = "samet#togkoer,defaultValue,minValue,maxValue,currentValue,unitName,unitCount#togkoer,defaultValue,minValue,maxValue,currentValue,unitName,unitCount#" ;
+            // Deserializing the string from Arduino
+            deSerialize(message);
 
-            // Split message by "#"
-            String[] parts = message.split("#");
-
-            //Set the Arduino name
-            arduino.setName(parts[0]);
-
-            //This method takes an array and converts it to a List object.
-            List<String> methods = new ArrayList<String>(Arrays.asList(parts));
-
-            //Removing the name
-            methods.remove(0);
-
-            // Creating a hashmap for group and core methods
-             Map<String, ArduinoMethod> groupMethods = new HashMap<>();
-             Map<String, ArduinoMethod> coreMethods = new HashMap<>();
-
-
-            for (String methodString : methods)
-            {
-                //Split var by ","
-                String [] var = methodString.split(",");
-
-                //checking currentState are null or empty
-                if(var[5] != null || var[5] != "")
-                {
-                    //If empty or null put into groupMethods
-                    groupMethods.put(var[0], new ArduinoMethod(var[0],Integer.parseInt(var[1]),Integer.parseInt(var[2]),Integer.parseInt(var[3]),Integer.parseInt(var[4]),var[5], var[6]));
-
-
-                }
-                else
-                {
-                    //Else put into coreMethods
-                    coreMethods.put(var[0], new ArduinoMethod(var[0],Integer.parseInt(var[1]),Integer.parseInt(var[2]),Integer.parseInt(var[3]),Integer.parseInt(var[4]),var[5], var[6]));
-                }
-
-            }
-            // Set core & groupmethods
-            arduino.setCoreMethods(coreMethods);
-            arduino.setGroupMethods(groupMethods);
-
-            //Get and Set the ip and converte the ip to a string
-            arduino.setIp(client.getSocket().getInetAddress().toString());
-
-            // Creating new hashmap for connectet arduino client
-            Map<String, Arduino> arduoinoClient = new HashMap<>();
-
-
-            arduoinoClient.put(arduino.getIp(), arduino);
-            // Get instance and set clients in arduinoClient
-            ArduinoServer.getInstance().setClients(arduoinoClient);
         }
         catch (Exception e)
         {
@@ -126,9 +76,61 @@ public class ThreadInit implements ThreadState {
         System.out.println("Can't talk until init is complete");
     }
 
-    private void deSerialize()
+    private void deSerialize(String message)
     {
+        Arduino arduino = new Arduino();
 
+        // Split message by "#"
+        String[] parts = message.split("#");
+
+        //Set the Arduino name
+        arduino.setName(parts[0]);
+
+        //This method takes an array and converts it to a List object.
+        List<String> methods = new ArrayList<String>(Arrays.asList(parts));
+
+        //Removing the name
+        methods.remove(0);
+
+        // Creating a hashmap for group and core methods
+        Map<String, ArduinoMethod> groupMethods = new HashMap<>();
+        Map<String, ArduinoMethod> coreMethods = new HashMap<>();
+
+
+        for (String methodString : methods)
+        {
+            //Split var by ","
+            String [] var = methodString.split(",");
+
+            //checking currentState are null or empty
+            if(var[5] != null || var[5] != "")
+            {
+                //If empty or null put into groupMethods
+                groupMethods.put(var[0], new ArduinoMethod(var[0],Integer.parseInt(var[1]),Integer.parseInt(var[2]),Integer.parseInt(var[3]),Integer.parseInt(var[4]),var[5], var[6]));
+
+
+            }
+            else
+            {
+                //Else put into coreMethods
+                coreMethods.put(var[0], new ArduinoMethod(var[0],Integer.parseInt(var[1]),Integer.parseInt(var[2]),Integer.parseInt(var[3]),Integer.parseInt(var[4]),var[5], var[6]));
+            }
+
+        }
+        // Set core & groupmethods
+        arduino.setCoreMethods(coreMethods);
+        arduino.setGroupMethods(groupMethods);
+
+        //Get and Set the ip and converte the ip to a string
+        arduino.setIp(client.getSocket().getInetAddress().toString());
+
+        // Creating new hashmap for connectet arduino client
+        Map<String, Arduino> arduoinoClient = new HashMap<>();
+
+
+        arduoinoClient.put(arduino.getIp(), arduino);
+        // Get instance and set clients in arduinoClient
+        ArduinoServer.getInstance().setClients(arduoinoClient);
 
     }
 
